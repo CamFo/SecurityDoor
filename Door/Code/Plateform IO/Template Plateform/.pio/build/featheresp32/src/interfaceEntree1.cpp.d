15d14e73379bcaa41b59f44148387439a96711ec
IlyesGml <128257644+IlyesGml@users.noreply.github.com>
2023-06-06 16:22:25 -0400

    new commit/merge iodk

---
+++ Door/Code/Plateform IO/Template Plateform/.pio/build/featheresp32/src/interfaceEntree1.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/featheresp32/src/interfaceEntree1.cpp.o: \
+ src/interfaceEntree1.cpp include/main.h include/piloteEntree1.h \
+ include/serviceBaseDeTemps.h include/interfaceEntree1.h
---
+++ Door/Code/Plateform IO/Template Plateform/.pio/build/featheresp32/src/interfaceT1.cpp.d
@@ -0,0 +1,2 @@
+.pio/build/featheresp32/src/interfaceT1.cpp.o: src/interfaceT1.cpp \
+ include/main.h include/piloteIOT1.h include/interfaceT1.h
